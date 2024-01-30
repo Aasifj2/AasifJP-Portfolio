b060789740b71e705c0e8b7969807dc24c51020c
heyaasif <aasif.javid@heyheyglobal.com>
2024-01-30 14:27:23 +0530

    date_changes

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -186,7 +186,7 @@ const GetStarted = () => {
           </div>
             <br3></br3>
             <h2 className="font1 sm:text-[24px] text-[20px]" style={{ textAlign:'center' }}> &nbsp;<span className="bold font1 sm:text-[26px] text-[22px]" style={{color:"hsla(295, 95%, 52%, 0.973)"}}> Research </span>And<span className="bold font1 sm:text-[26px] text-[22px]" style={{color:"hsla(295, 95%, 52%, 0.973)"}}> Development </span>Intern </h2>
-            <h2 className="font1 sm:text-[22px] text-[17px]" style={{ textAlign:'center'}}>APRIL 2022 <b><span style={{color:"hsla(295, 95%, 52%, 0.773)"}}>•</span>··</b>·············<b>··<span style={{color:"hsla(295, 95%, 52%, 0.773)"}}>•</span></b>DEC 2022</h2>
+            <h2 className="font1 sm:text-[22px] text-[17px]" style={{ textAlign:'center'}}>APRIL 2022 <b><span style={{color:"hsla(295, 95%, 52%, 0.773)"}}>•</span>··</b>·············<b>··<span style={{color:"hsla(295, 95%, 52%, 0.773)"}}>•</span></b>OCT 2022</h2>
             <p className="font1 sm:text-[22px] text-[17px]" style={{ textAlign:'center'}}> &nbsp;</p>
             <h2 className="font1 sm:text-[22px] text-[17px]" style={{ textAlign:'center' }}><b7>Tools and Technologies Used</b7></h2>
             <div className="hline-container" style={{ display: "flex", justifyContent: "center"}}>
